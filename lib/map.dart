2faff6734867792fd86a5ebdb9d11f18f1d0b9ef
alanta335 <alanta335@gmail.com>
2021-09-03 16:53:42 +0530

    direction working

--- lib/map.dart
+++ lib/map.dart
@@ -6,8 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_place/google_place.dart';
 import 'package:location/location.dart' as l;
-
-//import 'searchbar.dart';
+import 'package:google_maps_routes/google_maps_routes.dart';
 
 class MapScreen extends StatefulWidget {
   final lat, long;
@@ -22,17 +21,20 @@ class _MapScreenState extends State<MapScreen> {
   var la, lo;
   var result;
   var placeId;
+  int m = 0;
+  bool show = false;
   int countOfP = 0;
+  List<LatLng> points = [];
   l.Location location = new l.Location();
   _MapScreenState({@required this.la, @required this.lo});
   late GoogleMapController _controller;
-
-  Completer<GoogleMapController> _mapController = Completer();
+  late MapsRoutes route = new MapsRoutes();
   TextEditingController searchController = new TextEditingController();
   final Set<Marker> markers = {};
   late String queryS;
   TextEditingController searchQ = TextEditingController();
   var googlePlace = GooglePlace('AIzaSyA1bs9xDzhAEb5IpByX_-e0SzPW1QSXKQU');
+
   List<AutocompletePrediction> predictions = [];
   @override
   Widget build(BuildContext context) {
@@ -40,27 +42,76 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(fit: StackFit.expand, children: [
         buildMaps(),
         floatingSearch(),
+        Positioned(
+          bottom: 10,
+          right: 0,
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  _firLoc();
+                  _controller.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                        CameraPosition(target: LatLng(la, lo), zoom: 9)),
+                  );
+                },
+                child: Text('find friends'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  setState(() {
+                    markers.clear();
+                    route.routes.clear();
+                    markers.add(
+                      Marker(
+                        position: LatLng(la, lo),
+                        markerId: MarkerId('current_location'),
+                        infoWindow: InfoWindow(
+                            title: 'my location', snippet: '$la / $lo'),
+                      ),
+                    );
+                    points.clear();
+                    points.add(LatLng(la, lo));
+                  });
+                },
+                child: Text('clean routes'),
+              ),
+              ElevatedButton(
+                onPressed: () async {},
+                child: Text('add to trip'),
+              ),
+            ],
+          ),
+        ),
       ]),
-      floatingActionButton: FloatingActionButton(onPressed: () async {
-        _firLoc();
-        _controller.animateCamera(
-          CameraUpdate.newCameraPosition(
-              CameraPosition(target: LatLng(la, lo), zoom: 9)),
-        );
-      }),
     );
   }
 
-  void addMarker(cordinates) {
+  void addMarker(cordinates) async {
     print('$cordinates----+++++++');
+    points.add(cordinates);
+    print(points);
+    await route.drawRoute(points, 'trip', Color.fromRGBO(130, 78, 210, 1.0),
+        'AIzaSyA1bs9xDzhAEb5IpByX_-e0SzPW1QSXKQU',
+        travelMode: TravelModes.walking);
+    await Future.delayed(Duration(seconds: 4));
+    //this where thre should be loading
     setState(() {
       markers.add(
         Marker(
           position: cordinates,
-          markerId: MarkerId('orgin'),
-          infoWindow: InfoWindow(title: 'origin', snippet: 'starting position'),
+          markerId: MarkerId('$cordinates'),
+          infoWindow:
+              InfoWindow(title: '$cordinates', snippet: 'traveling marker'),
         ),
       );
+
+      m = 1;
+    });
+
+    print("RRRRRRRRROOOOOOOOOOOOUUUUUUUUTE" + route.toString());
+    setState(() {
+      m = 1;
     });
   }
 
@@ -111,26 +162,30 @@ class _MapScreenState extends State<MapScreen> {
             autoCompleteSearch(value);
           },
         ),
-        Container(
-          height: 300.0,
-          child: ListView.builder(
-            itemCount: countOfP,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text('${predictions.elementAt(index).description}'),
-                  onTap: () {
-                    searchController.text =
-                        predictions.elementAt(index).description!;
-                    setState(() {
-                      countOfP = 0;
-                      placeId = predictions.elementAt(index).placeId;
-                      print(placeId);
-                      _goToPlace(placeId);
-                    });
-                  });
-            },
-          ),
-        ),
+        show
+            ? Container(
+                height: 300.0,
+                child: ListView.builder(
+                  itemCount: countOfP,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                        title:
+                            Text('${predictions.elementAt(index).description}'),
+                        onTap: () {
+                          searchController.text =
+                              predictions.elementAt(index).description!;
+                          setState(() {
+                            countOfP = 0;
+                            placeId = predictions.elementAt(index).placeId;
+                            print(placeId);
+                            show = false;
+                            _goToPlace(placeId);
+                          });
+                        });
+                  },
+                ),
+              )
+            : Container(),
       ],
     );
   }
@@ -139,10 +194,13 @@ class _MapScreenState extends State<MapScreen> {
     return GoogleMap(
       myLocationButtonEnabled: false,
       zoomControlsEnabled: false,
+      polylines: route.routes,
       initialCameraPosition: CameraPosition(target: LatLng(la, lo), zoom: 19),
       onMapCreated: (controller) {
         setState(() {
           _controller = controller;
+          points.add(LatLng(la, lo));
+          print(points.toString());
           markers.add(
             Marker(
               position: LatLng(la, lo),
@@ -154,8 +212,10 @@ class _MapScreenState extends State<MapScreen> {
         });
       },
       markers: markers.toSet(),
-      onLongPress: (cordinates) {
-        addMarker(cordinates);
+      onLongPress: (cordinates) async {
+        setState(() {
+          addMarker(cordinates);
+        });
       },
       onTap: (cordinates) {
         _controller.animateCamera(CameraUpdate.newLatLng(cordinates));
@@ -182,6 +242,7 @@ class _MapScreenState extends State<MapScreen> {
     if (result != null && result.predictions != null && mounted) {
       setState(() {
         predictions = result.predictions!;
+        show = true;
         print(predictions);
       });
       predictions.map((
