e8746e47a3997bcf2b18d5403a4002c69df04aa4
ananthu <[ananthuvngmkd@gmail.com]>
2021-09-04 11:26:52 +0530

    Co-authored-by: Bageeradhan <11bageeradhan9@gmail.com>

--- lib/weatherUI.dart
+++ lib/weatherUI.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weather/weather.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 var loc;
+var w;
 
 class Weatherui extends StatefulWidget {
   var locality;
@@ -12,27 +14,77 @@ class Weatherui extends StatefulWidget {
 }
 
 class _WeatheruiState extends State<Weatherui> {
+  _WeatheruiState({required this.loc});
   var loc;
+  List<Weather> _data = [];
   WeatherFactory wf = new WeatherFactory("f6f05e62a44e4f9ba8eb4b805ef44e74");
-  void queryWeather() async {
+
+  Future<Weather> queryWeather() async {
     Weather w = await wf.currentWeatherByCityName(loc);
-    print(w.tempMax);
+    print(w.areaName);
+    return w;
   }
 
   void fivedayWeather() async {
     List<Weather> forecast = await wf.fiveDayForecastByCityName(loc);
+    print(forecast);
+    _data = forecast;
   }
-
-  _WeatheruiState({required this.loc});
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-    queryWeather();
+    double width = MediaQuery.of(context).size.width;
+    w = queryWeather();
     fivedayWeather();
     return Scaffold(
       body: Column(
-        children: [],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(37, 36, 39, 1),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            width: width * .8,
+            height: height * 0.2,
+            child: Row(
+              children: <Widget>[
+                Text(_data.first.areaName!),
+                BoxedIcon(
+                  WeatherIcons.cloudy,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    'Check out the whole forecast',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color.fromRGBO(37, 36, 39, 1),
+                    ),
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
+                  ),
+                ]),
+          )
+        ],
       ),
     );
   }
